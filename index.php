ef5c60d6797954cfcac667abe6195e34cf8f7b5d
natsuki <natsuki.uji410@gmail.com>
2023-05-01 13:53:20 +0900

    課題①-1完了しました

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP課題</title>
 </head>
 <body>
-    test
+  <p>日本の首都は？</p>  
+<form action="index.php" method="POST">
+    <input type="text" name="text">
+    <input type="submit" value="OK">
+    <br>
+<?php
+$input_data = $_POST['text'];
+if ($input_data == '東京'){
+  echo '正解';
+}else{
+  echo '不正解';
+}
+?>
+</form>
 </body>
 </html>
